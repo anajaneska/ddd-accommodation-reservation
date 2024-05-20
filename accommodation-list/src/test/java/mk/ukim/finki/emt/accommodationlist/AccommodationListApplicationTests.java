e5027062ab5601d49a8fa65d9dd974662718b94b
anajaneska <anajaneska1@outlook.com>
2024-05-20 19:02:04 +0200

    test

---
+++ accommodation-list/src/test/java/mk/ukim/finki/emt/accommodationlist/AccommodationListApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.accommodationlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
