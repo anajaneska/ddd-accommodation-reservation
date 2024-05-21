394c7654ee10fcc865ca86fa53ab3232a3001ba1
anajaneska <anajaneska1@outlook.com>
2024-05-21 21:32:11 +0200

    reservation and accommodation

---
+++ reservation-management/src/main/java/mk/ukim/finki/emt/reservationmanagemnet/ReservationManagemnetApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.reservationmanagemnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationManagemnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationManagemnetApplication.class, args);
+	}
+
+}
---
+++ reservation-management/src/test/java/mk/ukim/finki/emt/reservationmanagemnet/ReservationManagemnetApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.reservationmanagemnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagemnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
